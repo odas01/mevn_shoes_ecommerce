0b9f5ab930d6b9b359520e03b3d651cac0cfe30c
lntthanh3317 <lntthanh3317@gmail.com>
2023-03-28 20:38:57 +0700

    setting vercel

--- backend/index.js
+++ backend/index.js
@@ -27,23 +27,6 @@ app.use("/carts", cartRouter);
 app.use("/orders", orderRouter);
 app.use("/users", userRouter);
 
-// handle 404 response
-app.use((req, res, next) => {
-  // Code ở đây sẽ chạy khi không có route được định nghĩa nào
-  // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
-  return next(new ApiError(404, "Resource not found"));
-});
-
-// define error-handling middleware last, after other app.use() and routes calls
-app.use((err, req, res, next) => {
-  // Middleware xử lý lỗi tập trung.
-  // Trong các đoạn code xử lý ở các route, gọi next(error)
-  // sẽ chuyển về middleware xử lý lỗi này
-  return res.status(err.statusCode || 500).json({
-    message: err.message || "Internal Server Error",
-  });
-});
-
 const port = config.app.port || 5000;
 
 app.listen(port, () => {
