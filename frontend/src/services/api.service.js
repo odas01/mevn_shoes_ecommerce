f86aab638f7bee5e69dee86924395553374e58cd
lntthanh3317 <lntthanh3317@gmail.com>
2023-03-29 00:13:31 +0700

    setting vercel

--- frontend/src/services/api.service.js
+++ frontend/src/services/api.service.js
@@ -1,13 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 function createApiClient(baseURL) {
-    return axios.create({
-        baseURL,
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-        },
-    });
+  const newBaseURL = "https://mevn-nikeshoes-api.vercel.app/" + baseURL;
+  return axios.create({
+    newBaseURL,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
 }
 
 export default createApiClient;
