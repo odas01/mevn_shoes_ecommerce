9dfc6693f9c06b3a65f6850f07cb15132cbb21a6
lntthanh3317 <lntthanh3317@gmail.com>
2023-03-28 20:56:27 +0700

    setting vercel

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,17 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 
-import config from "./app/config/index.js";
 import productRouter from "./app/routes/product.route.js";
 import categoryRouter from "./app/routes/category.route.js";
 import cartRouter from "./app/routes/cart.route.js";
 import orderRouter from "./app/routes/order.route.js";
 import userRouter from "./app/routes/user.route.js";
-import ApiError from "./app/api-error.js";
+
+import * as dotenv from "dotenv";
+dotenv.config();
 
 // Connect mongodb by mongoose
-mongoose.connect(config.db.uri, () => {
+mongoose.connect(process.env.DB, () => {
   console.log("Connect to MongoDB successfully!!!");
 });
 
@@ -27,7 +28,7 @@ app.use("/carts", cartRouter);
 app.use("/orders", orderRouter);
 app.use("/users", userRouter);
 
-const port = config.app.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
