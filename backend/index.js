7b78a47def35a67b9fdb905334a5384c0938758a
lntthanh3317 <lntthanh3317@gmail.com>
2023-03-28 20:17:26 +0700

    setting vercel

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,10 @@ app.use(cors());
 app.use(express.json({ limit: "70mb" }));
 app.use(express.urlencoded({ limit: "70mb", extended: true }));
 
+app.get("/", (req, res) => {
+  res.json({ message: "Hello Products!" });
+});
+
 // routes
 app.use("/products", productRouter);
 app.use("/categorys", categoryRouter);
